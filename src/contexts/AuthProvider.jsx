2efda0550cfe47b87a97c0ab110bb3a606e92ec4
AhmedAtri <ahmed.atri@isimsf.u-sfax.tn>
2024-12-18 07:41:03 +0100

    final

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import API from "../utils/API";
 import axios from "axios";
-// import axios, { setToken } from "../lib/axiosInstance";
 
 const AuthContext = React.createContext();
 
@@ -9,9 +7,8 @@ function AuthProvider({ children }) {
   const [user, setUser] = useState();
 
   async function getUserStatus() {
-    // setToken(localStorage.getItem("ELCAMBA_token"));
+    
     try {
-      const res = await axios.get("/login/status");
       setUser(res.data.user);
       return res;
     } catch (err) {
